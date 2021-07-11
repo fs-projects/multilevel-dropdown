a7b29f16aaab3e55a73d8f67b3c3f9384765bf39
fs-projects <fs.projectsuk@gmail.com>
2021-07-11 10:34:39 +0530

    Add: Multilevel drop down files

--- multi-dropdown/scripts.js
+++ multi-dropdown/scripts.js
@@ -1,3 +1,8 @@
 //Create function to select elements
-
+const hamburgerIcon = document.querySelector(".menu-icons");
+const navElement = document.querySelector("nav");
 //Open and close nav on click
+
+hamburgerIcon.addEventListener("click", () => {
+  navElement.classList.toggle("active");
+});
